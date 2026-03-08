99aba4a9a19ee53b13e8951242e3be8940589170
Luca Bertelli <bertelli.luca@proton.me>
2026-03-08 12:10:58 +0100

    ci: Fix CI pipelines

---
+++ frontend/eslint.config.js
@@ -0,0 +1,16 @@
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import reactHooks from "eslint-plugin-react-hooks";
+
+export default [
+  ...tsPlugin.configs["flat/recommended"],
+  {
+    files: ["src/**/*.ts", "src/**/*.tsx"],
+    plugins: {
+      "react-hooks": reactHooks,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+    },
+  },
+];
